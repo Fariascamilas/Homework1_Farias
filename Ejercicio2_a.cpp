a86928f9b7d24ec88f5de007b66965332ae98521
Fariascamilas <cfarias@udesa.edu.ar>
2025-03-16 17:46:40 -0300

    Ejercicio 2 en proceso

---
+++ Ejercicio2_a.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <limits>
+
+using namespace std;
+
+#define DEBUG 1
+#define INFO 2
+#define WARNING 3
+#define ERROR 4
+#define CRITICAL 5
+
+void logMessage(string message, int severity_level){
+    ofstream archive("LogSystem", ios::app);
+    if(!archive){
+        cout<<"Error de apertura de archivo"<<endl;
+        return;
+    }
+    
+    switch(severity_level){
+        case DEBUG:
+            archive<<"["<<"DEBUG"<<"]"<<"<"<<message<<">"<<endl;
+            break;
+        case INFO:
+            archive<<"["<<"INFO"<<"]"<<"<"<<message<<">"<<endl;
+            break;
+        case WARNING:
+            archive<<"["<<"WARNING"<<"]"<<"<"<<message<<">"<<endl;
+            break;
+        case ERROR:
+            archive<<"["<<"ERROR"<<"]"<<"<"<<message<<">"<<endl;
+            break;
+        case CRITICAL:
+            archive<<"["<<"CRITICAL"<<"]"<<"<"<<message<<">"<<endl;
+            break;
+    }
+    archive.close();
+}
+
+int main(void){
+    string message;
+    int level;
+    cout<<"Ingrese un mensaje"<<endl;
+    getline(cin,message); // Lee una linea completa
+    cout<<"Ingrese nivel de severidad: (1:DEBUG, 2:INFO, 3:WARNING, 4:ERROR, 5:CRITICAL)"<<endl;
+    cin>>level;
+    logMessage(message,level);
+    return 0;
+}
