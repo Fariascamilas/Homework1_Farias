c14dc5fab5697651fcef31c5b386d63c95756fe4
Fariascamilas <cfarias@udesa.edu.ar>
2025-03-18 19:23:28 -0300

    Avances Ejercicio 3

---
+++ Ejercicio3.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+struct node{
+    shared_ptr<node> next;
+    int value;
+};
+
+struct list{
+    shared_ptr<node> head;
+    int size;
+};
+
+// Elijo usar shared pointer ya que, por ejemplo, a la hora de hacer push back no es posible iterar con punteros de tipo unique lo que implica el uso de raw pointers que son mas inseguros
+
+shared_ptr<node> create_node(int value){
+    unique_ptr<node> newNode(new node);
+    newNode->next = nullptr;
+    newNode->value = value;
+    return newNode;
+}
+
+void push_front(shared_ptr<list> l, int val){
+    shared_ptr<node> newNode = create_node(val);
+    // No chequeo si l esta vacia ya que de todos modos head pasa a ser newNode y newNode sigue apuntando a nullptr
+    newNode->next = move(l->head);
+    l->head = move(newNode);
+    l->size++;
+}
+
+void push_back(shared_ptr<list> l, int val){
+    shared_ptr<node> newNode = create_node(val);
+    if(l->size == 0){
+        l->head = move(newNode);
+        l->size++;
+        return;
+    }
+
+    shared_ptr<node> current = l->head; // Previamente ya chequee si la lista esta vacia para que current no sea nullptr
+
+    while(current->next){
+        current = current->next;
+    }
+    current->next = move(newNode);
+    l->size++;
+}
+
+void insert(shared_ptr<list> l, int val, int pos){
+    if (pos < 0){
+        cout<<"Error: la posicion ingresada es menor a cero"<<endl;
+        return;
+    }
+    
+    if(l->size == 0){
+        push_back(l,val);
+        if(pos != 0){
+            cout<<"La posicion ingresada no existe. El valor fue insertado al final de la lista"<<endl;
+        }
+        return;
+    }
+    if (pos == 0){
+        push_front(l,val);
+        return;
+    }
+    shared_ptr<node> current = l->head;
+    
+    for(int i = 0; i<(l->size-1); i++){
+        if(i == (pos-1)){
+            shared_ptr<node> newNode = create_node(val);
+            current->next = move(newNode);
+            l->size++;
+            return;
+        }
+        current = current->next;
+    }
+    // Si no lo inserto anteriormente:
+    push_back(l,val);
+    cout<<"La posicion ingresada no existe. El valor fue insertado al final de la lista"<<endl;
+}
+
+void erase(shared_ptr<list> l, int pos){// La posicion inicial es cero
+    if(l->size == 0){
+        cout<<"La lista esta vacia. No hay valores para borrar"<<endl;
+        return;
+    }
+
+    if (pos == 0){
+        if(l->head->next){l->head = move(l->head->next);}
+        else{l->head = nullptr;}
+        l->size--;
+        return;
+    }
+
+    shared_ptr<node> current = l->head;
+    for(int i = 0; i<(l->size-1); i++){ // Quiero ir hasta la ante ultima posicion para guardarme ese nodo en caso de que pos sea mayor al largo (util para borrar el ultimo)
+        
+        if(i == (pos-1)){
+            if(current->next->next){
+                current->next = move(current->next->next);
+            }
+            else{
+                current->next = nullptr;
+            }
+            l->size--;
+            return;
+        }
+        current = current->next;
+    }
+
+    // Si no se borro un elemento antes:
+    current->next = nullptr;
+    l->size--;
+    cout<<"La posicion ingresada no existe. Se borro el ultimo valor de la lista"<<endl;
+}
+
+void print_list(shared_ptr<list> l){
+    shared_ptr<node> current = l->head;
+    while(current){
+        cout<<current->value;
+        if(current->next != nullptr){
+            cout<<"->";
+        }
+        current = current->next;
+    }
+    cout<<"-> nullptr"<<endl;
+}
